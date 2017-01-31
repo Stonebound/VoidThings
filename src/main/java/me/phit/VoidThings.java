7db2be8e0e99c5eb9971edb4030a9e4160e08a18
phit <phit@phit.link>
2017-01-31 02:38:02 +0100

    copypaste is strong

--- src/main/java/me/phit/VoidThings.java
+++ src/main/java/me/phit/VoidThings.java
@@ -3,16 +3,10 @@ package me.phit;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
-import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.format.TextStyles;
 import org.spongepowered.api.world.gen.WorldGeneratorModifier;
 
 @Plugin(
@@ -26,7 +20,6 @@ public class VoidThings {
 
     @Inject
     private Logger logger;
-
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
